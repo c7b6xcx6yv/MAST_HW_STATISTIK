90f4ca25333dbd26f83a9119d7c798367ce46034
c7b6xcx6yv <c7b6xcx6yv@privaterelay.appleid.com>
2025-10-17 21:11:00 +0200

    init

---
+++ MAST_HW_STAT.R
@@ -0,0 +1,10 @@
+mailSize <- c(1.5, 0.9, 3.3, 9.6, 6.1, 1.6, 2.1, 3.8, 5.3, 2.6, 7.1, 1.7, 1.0, 4.9)
+
+# Berechne das arithmetische Mittel der Dateigrößen
+mean(mailSize)
+
+# Berechne die Stichprobenvarianz der Dateigrößen
+var(mailSize)
+
+# Berechne die in der Stichprobe gegebene Standardabweichung der Dateigrößen
+sd(mailSize)
